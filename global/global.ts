66535ed7d4cb3deca1eba6053186b145523296e7
Jannis Pohlmann <contact@jannispohlmann.de>
2018-11-02 12:19:14 +0100

    global: Comment out the allocate_memory export for now, as it breaks builds

--- global/global.ts
+++ global/global.ts
@@ -1,2 +1,5 @@
-import "allocator/arena";
-export { allocate_memory };
+// TODO: Figure out a way to essentially do the following, so mappings
+// don't have to do it themselves:
+//
+// import 'allocator/arena'
+// export { allocate_memory }
