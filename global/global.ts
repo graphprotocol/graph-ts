cd7e5b79da9b4ee0f6920af024924b098debf8d5
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2018-11-28 19:15:45 -0200

    global: Uncomment globals.ts
    It will now be appended to mappings.

--- global/global.ts
+++ global/global.ts
@@ -1,5 +1,2 @@
-// TODO: Figure out a way to essentially do the following, so mappings
-// don't have to do it themselves:
-//
-// import 'allocator/arena'
-// export { allocate_memory }
+import 'allocator/arena'
+export { allocate_memory }
