a726bfbb779f634af641c5bb718b47750e54a7ec
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2019-01-14 22:07:35 -0200

    test: Fix tests, remove noTreeShaking
    After the asc bump noTreeShaking started causing
    issues, causing the test to fail. First, now test failures
    will actually fail CI. Second, remove noTreeShaking
    which makes the test almost useless, but we
    can gradually make it useful again.

--- test/test.ts
+++ test/test.ts
@@ -1,1 +1,7 @@
-import 'temp_lib/index'
+import { ByteArray } from 'temp_lib/index'
+
+// `export` so this isn't considered dead code.
+export function foo(): void {
+    let a = new ByteArray(0)
+    let b: string = a.toBase58()
+}
