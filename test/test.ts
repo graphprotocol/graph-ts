74471217e1bdfb90f6cccb4af50e942505175d66
Leonardo Yvens <leoyvens@gmail.com>
2019-05-10 18:41:05 -0300

    index: BigInt comparison in AS
    Resolves #35.
    Part of #47.
    
    Adds ByteArray.fromI32.
    Re-implements BigInt.fromI32 in AS.

--- test/test.ts
+++ test/test.ts
@@ -2,13 +2,26 @@ import { BigInt, ByteArray, Bytes } from 'temp_lib/index'
 
 // Test some BigInt methods.
 export function test(): void {
-    let bytes = new ByteArray(4)
-    bytes[0] = 5
-    bytes[1] = 0
-    let five = BigInt.fromSignedBytes(bytes as Bytes)
-    assert(!five.isZero())
+    let minus_five_bytes = new ByteArray(2)
+    minus_five_bytes[0] = 251
+    minus_five_bytes[1] = 255
+    let minus_five = BigInt.fromSignedBytes(minus_five_bytes as Bytes)
+    assert(minus_five.isI32())
+    assert(minus_five == BigInt.fromI32(-5))
+    assert(!minus_five.isZero() && minus_five.isI32())
+
+    let five_bytes = new ByteArray(2)
+    five_bytes[0] = 5
+    five_bytes[1] = 0
+    let five = BigInt.fromSignedBytes(five_bytes as Bytes)
+    assert(!five.isZero() && five.isI32())
+    assert(five == BigInt.fromI32(5))
+    assert(five != minus_five)
 
     let zero = BigInt.fromSignedBytes(new ByteArray(0) as Bytes)
-    assert(zero.isZero())
+    assert(zero.isZero() && zero.isI32())
     assert(zero != five)
+    assert(zero != minus_five)
+    assert(minus_five < zero && minus_five <= zero)
+    assert(five > zero && five >= zero)
 }
