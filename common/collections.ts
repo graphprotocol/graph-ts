6c1f7e091e304bc07c8abbfbbadcd6e38aefb9e9
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-10-27 14:35:22 -0400

    Fixed majority of comments

--- common/collections.ts
+++ common/collections.ts
@@ -22,7 +22,7 @@ export class ByteArray extends Uint8Array {
    * Returns bytes in little-endian order.
    */
    static fromU32(x: u32): ByteArray {
-    let self = new ByteArray(8)
+    let self = new ByteArray(4)
     self[0] = x as u8
     self[1] = (x >> 8) as u8
     self[2] = (x >> 16) as u8
@@ -171,7 +171,7 @@ export class ByteArray extends Uint8Array {
    * Throws in case of overflow.
    */
 
-   toI64(): i64 {
+  toI64(): i64 {
     let isNeg = this.length > 0 && this[this.length - 1] >> 7 == 1
     let padding = isNeg ? 255 : 0
     for (let i = 8; i < this.length; i++) {
@@ -210,35 +210,35 @@ export class ByteArray extends Uint8Array {
    */
 
   toU64(): u64 {
-      for (let i = 8; i < this.length; i++) {
-        if (this[i] != 0) {
-          assert(false, 'overflow converting ' + this.toHexString() + ' to u64')
-        }
+    for (let i = 8; i < this.length; i++) {
+      if (this[i] != 0) {
+        assert(false, 'overflow converting ' + this.toHexString() + ' to u64')
       }
-      let paddedBytes = new Bytes(8)
-      paddedBytes[0] = 0
-      paddedBytes[1] = 0
-      paddedBytes[2] = 0
-      paddedBytes[3] = 0
-      paddedBytes[4] = 0
-      paddedBytes[5] = 0
-      paddedBytes[6] = 0
-      paddedBytes[7] = 0
-      let minLen = paddedBytes.length < this.length ? paddedBytes.length : this.length
-      for (let i = 0; i < minLen; i++) {
-        paddedBytes[i] = this[i]
-      }
-      let x: u64 = 0
-      x = (x | paddedBytes[7]) << 8
-      x = (x | paddedBytes[6]) << 8
-      x = (x | paddedBytes[5]) << 8
-      x = (x | paddedBytes[4]) << 8
-      x = (x | paddedBytes[3]) << 8
-      x = (x | paddedBytes[2]) << 8
-      x = (x | paddedBytes[1]) << 8
-      x = x | paddedBytes[0]
-      return x
     }
+    let paddedBytes = new Bytes(8)
+    paddedBytes[0] = 0
+    paddedBytes[1] = 0
+    paddedBytes[2] = 0
+    paddedBytes[3] = 0
+    paddedBytes[4] = 0
+    paddedBytes[5] = 0
+    paddedBytes[6] = 0
+    paddedBytes[7] = 0
+    let minLen = paddedBytes.length < this.length ? paddedBytes.length : this.length
+    for (let i = 0; i < minLen; i++) {
+      paddedBytes[i] = this[i]
+    }
+    let x: u64 = 0
+    x = (x | paddedBytes[7]) << 8
+    x = (x | paddedBytes[6]) << 8
+    x = (x | paddedBytes[5]) << 8
+    x = (x | paddedBytes[4]) << 8
+    x = (x | paddedBytes[3]) << 8
+    x = (x | paddedBytes[2]) << 8
+    x = (x | paddedBytes[1]) << 8
+    x = x | paddedBytes[0]
+    return x
+  }
 
   @operator('==')
   equals(other: ByteArray): boolean {
